61a6492868d4076bd48a5c78db3e3e10a8bfd012
vwx <vistar_w@hotmail.com>
2022-06-22 11:34:42 -0400

    add feature

--- zsh/.vwz/plugins/vwstartup/custom.sh
+++ zsh/.vwz/plugins/vwstartup/custom.sh
@@ -6,6 +6,6 @@ echo -en "custom script running...\n"
 nohup sudo xkeysnail --watch --quiet /home/vwx/.config/xkeysnail/vwiss-emacs-hhkb.py >> /dev/null 2>&1 &
 nohup sudo /usr/local/trojan/trojan /usr/local/trojan/config.json >> /dev/null 2>&1 &
 
-emacs
+tmux new-window && nohup emacs &
 
 alias vwks="sudo pkill trojan && sudo pkill xkeysnail"
